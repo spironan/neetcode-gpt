9baa489b1fb0758be5d1f710b1640ff66e1cf7eb
Chua Teck Lee <spironan@users.noreply.github.com>
2026-05-13 16:30:09 +0800

    Update NLP preprocessing (NLP 2/4)

--- data/nlp_preprocessing.py
+++ data/nlp_preprocessing.py
@@ -15,20 +15,8 @@ class Solution:
 
         combine = positive + negative
         for sentence in combine:
-            row = []
-            words = sentence.split()
-            for word in words:
-                row.append(lookup[word])
-
+            row = [lookup[word] for word in sentence.split()]
             output.append(torch.tensor(row))
-                
-        # for sentence in negative:
-        #     row = []
-        #     words = sentence.split()
-        #     for word in words:
-        #         row.append(lookup[word])
-
-        #     output.append(torch.tensor(row))
         
         # finally we pad shorter sequences with 0s
         output = nn.utils.rnn.pad_sequence(output, batch_first=True)
